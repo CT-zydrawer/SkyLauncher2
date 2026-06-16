9d7ccd81b4b6d19cd6bcd0622876aa1c8480159a
drawer zy <13375002870@163.com>
2026-06-16 22:17:38 +0800

    界面修复

--- Views/DownloadPage.axaml.cs
+++ Views/DownloadPage.axaml.cs
@@ -10,4 +10,33 @@ public partial class DownloadPage : UserControl
         InitializeComponent();
         DataContext = new DownloadViewModel();
     }
+
+    private void TabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+        var tabControl = sender as TabControl;
+        var selectedTabItem = tabControl?.SelectedItem as TabItem;
+
+        if (selectedTabItem != null)
+        {
+
+            var transitionControl = FindTransitioningContentControl(selectedTabItem);
+
+            if (transitionControl != null)
+            {
+                var content = transitionControl.Content;
+                transitionControl.Content = null;
+                transitionControl.Content = content;
+            }
+        }
+    }
+
+    private TransitioningContentControl? FindTransitioningContentControl(TabItem tabItem)
+    {
+        if (tabItem.Content is TransitioningContentControl transition)
+        {
+            return transition;
+        }
+        return null;
+    }
 }
--- Views/ToolboxPage.axaml.cs
+++ Views/ToolboxPage.axaml.cs
@@ -192,5 +192,34 @@ namespace SkyLauncher.Views
             }
         }
 
+        private void MainTabControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+  
+            var tabControl = sender as TabControl;
+            var selectedTabItem = tabControl?.SelectedItem as TabItem;
+
+            if (selectedTabItem != null)
+            {
+                
+                var transitionControl = FindTransitioningContentControl(selectedTabItem);
+
+                if (transitionControl != null)
+                {
+                    var content = transitionControl.Content;
+                    transitionControl.Content = null;
+                    transitionControl.Content = content;
+                }
+            }
+        }
+
+        private TransitioningContentControl? FindTransitioningContentControl(TabItem tabItem)
+        {
+            if (tabItem.Content is TransitioningContentControl transition)
+            {
+                return transition;
+            }
+            return null;
+        }
+
     }
 }
